4b8502f75d1c0f7dc66e973ebc7db98c1e4e03ff
ms <onlyszyou87@naver.com>
2020-02-12 12:45:52 +0900

    런치박스 컨트롤러추가

---
+++ Lunch_Box/src/main/java/com/kh/team2/LunchBoxController.java
@@ -0,0 +1,27 @@
+package com.kh.team2;
+
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+@RequestMapping("/lb")
+public class LunchBoxController {
+	
+	@RequestMapping(value = "/join")
+	public String join() {
+		
+		
+		return "join";
+	}
+	@RequestMapping(value = "/login")
+	public String login() {
+		
+		
+		return "login";
+	}
+	
+}
