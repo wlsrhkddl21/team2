cd240b4137035be537c6cdd11bfd47c9a0c38508
kwon <guen1852@naver.com>
2020-02-13 10:27:48 +0900

    현기 진짜 삭제

--- Lunch_Box/src/main/java/com/kh/team2/persistence/MemberDao.java
+++
@@ -1,8 +0,0 @@
-package com.kh.team2.persistence;
-
-import com.kh.team2.domain.MemberVo;
-
-public interface MemberDao {
-
-	public void insertMember(MemberVo memberVo);
-}
--- Lunch_Box/src/main/java/persistence/MemberDao.java
+++
@@ -1,8 +0,0 @@
-package persistence;
-
-import com.kh.team2.domain.MemberVo;
-
-public interface MemberDao {
-
-	public void insertMember(MemberVo memberVo);
-}
