c0e2296a81827ef46d9be0221766f7ebf5caafcb
ms <onlyszyou87@naver.com>
2020-02-18 16:29:25 +0900

    댓글 수정삭제 작업중

--- Lunch_Box/src/main/java/com/kh/team2/service/NtReplyService.java
+++ Lunch_Box/src/main/java/com/kh/team2/service/NtReplyService.java
@@ -10,4 +10,8 @@ public interface NtReplyService {
 	public void create(ntReplyVo ntreReplyVo) throws Exception;
 	// 댓글목록
 	public List<ntReplyVo> list(int ntbno) throws Exception;
+	// 댓글수정
+	public void update(ntReplyVo ntreplyVo) throws Exception;
+	// 댓글 삭제
+	public void delete(int ntrno) throws Exception;
 }
